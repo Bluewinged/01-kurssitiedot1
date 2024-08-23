88c93fa35c16b66d7acba5683c0c3c5320e71383
Bluewinged <kiki.kokkonen@outlook.com>
2024-08-23 11:58:04 +0300

    content osa valmis

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,34 @@
+const Header = (props) => {
+  console.log(props);
+  return (
+    <>
+      <h1>Hei, olet kurssilla {props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        Osan {props.name} tehtävien määrä on {props.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <section>
+        <Part name={props.part1} exercises={props.exercises1} />
+        <Part name={props.part2} exercises={props.exercises2} />
+        <Part name={props.part3} exercises={props.exercises3} />
+      </section>
+    </>
+  );
+};
+
 const App = () => {
   const course = "Half Stack application development";
   const part1 = "Fundamentals of React";
@@ -10,15 +41,15 @@ const App = () => {
   return (
     <>
       <Header course={course} />
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </>
   );
