e209ec3720e908302069f71e7fd62de4561681a9
Bluewinged <kiki.kokkonen@outlook.com>
2024-09-13 10:37:41 +0300

    osat 1.4 ja 1.5 valmis

--- src/App.jsx
+++ src/App.jsx
@@ -40,25 +40,39 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
   };
 
   return (
     <>
-      <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Header course={course.name} />
+      <Content
+        part1={course.parts[0]}
+        part2={course.parts[1]}
+        part3={course.parts[2]}
+      />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
     </>
   );
 };
